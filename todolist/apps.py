3f62ec3aca52e8863a02b4874427c51606a1e9c0
Ali <aliahmadi.xx90@gmail.comOmit>
2024-06-17 18:56:06 +0330

    I add signal to this file

--- todolist/apps.py
+++ todolist/apps.py
@@ -5,3 +5,6 @@ class TodolistConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'todolist'
     verbose_name = 'تو دو اپ'
+
+    def ready(self):
+        import todolist.signals
