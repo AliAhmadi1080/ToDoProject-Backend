56ffdfbd2ffdba61b4e75a1785285e64d6d90c07
Ali <aliahmadi.xx90@gmail.comOmit>
2024-06-19 01:31:08 +0330

    I write view for list of todos and detail of one todo

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .serializers import ToDoSirializer
+from .models import ToDo
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 
-# Create your views here.
+@api_view(['GET'])
+def list_todo(request:Request):
+    todos = ToDo.objects.all()
+    
+    serialized_todos = ToDoSirializer(todos,many=True)
+    todos_data = serialized_todos.data
+    return Response(todos_data,HTTP_200_OK)
+
+
+@api_view(['GET'])
+def detail_todo(request:Request):
+    try:
+        payload = int(request.query_params.get('id'))
+    except:
+        return Response(None,HTTP_400_BAD_REQUEST)
+    print(payload)
+    
+    
+    todos = ToDo.objects.filter(id=int(payload))
+    
+    serialized_todos = ToDoSirializer(todos,many=True)
+    todos_data = serialized_todos.data
+    return Response(todos_data,HTTP_200_OK)
+
+
+
