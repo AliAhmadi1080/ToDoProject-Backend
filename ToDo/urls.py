fe57be22da60c263c2cddf0be3e9d2cb954e7d62
Ali <aliahmadi.xx90@gmail.comOmit>
2024-06-19 01:28:50 +0330

    I add todolist.urls path to this file

--- ToDo/urls.py
+++ ToDo/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+
+    path('todo/', include('todolist.urls')),
 ]
