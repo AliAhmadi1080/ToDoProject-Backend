6103ca33834a8da0cd69d0e462ef8e3753a840af
Ali <aliahmadi.xx90@gmail.comOmit>
2024-06-19 02:36:24 +0330

    I create CHANGE DELETE and CREATE api with path

--- todolist/urls.py
+++ todolist/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('todolist/',views.list_todo),
-    path('detailtodo/',views.detail_todo),
+    path('listtodo/',views.list_todo),
+    path('detailtodo/<int:pk>',views.detail_todo),
+    path('createtodo/',views.create_todo),
+    path('changetodo/<int:pk>',views.change_todo),
+    path('deletetodo/<int:pk>',views.delete_todo),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .serializers import ToDoSirializer
 from .models import ToDo
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.status import *
 
 @api_view(['GET'])
 def list_todo(request:Request):
@@ -16,19 +16,46 @@ def list_todo(request:Request):
 
 
 @api_view(['GET'])
-def detail_todo(request:Request):
-    try:
-        payload = int(request.query_params.get('id'))
-    except:
-        return Response(None,HTTP_400_BAD_REQUEST)
-    print(payload)
+def detail_todo(request:Request,pk:int): 
+    todos = get_object_or_404(ToDo,pk=pk)
     
-    
-    todos = ToDo.objects.filter(id=int(payload))
-    
-    serialized_todos = ToDoSirializer(todos,many=True)
+    serialized_todos = ToDoSirializer(todos)
     todos_data = serialized_todos.data
     return Response(todos_data,HTTP_200_OK)
 
+@api_view(['POST'])
+def create_todo(request:Request):
+    if request.method == 'POST':
+        instance = ToDoSirializer(data=request.data,many=False)
+        if instance.is_valid():
+            instance.save()
+            return Response(instance.data,HTTP_201_CREATED)
+        return Response(None,HTTP_400_BAD_REQUEST)
 
+    if request.method == "GET":
+        return Response(None,HTTP_405_METHOD_NOT_ALLOWED)
+    
+@api_view(['PUT'])
+def change_todo(request:Request,pk:int):
+    if request.method == 'PUT':
+        instance = get_object_or_404(ToDo,pk=pk)
+        instance = ToDoSirializer(instance,data=request.data,many=False)
+        if instance.is_valid():
+            instance.save()
+            return Response(instance.data,HTTP_201_CREATED)
+        return Response(None,HTTP_400_BAD_REQUEST)
 
+    if request.method == "GET":
+        return Response(None,HTTP_405_METHOD_NOT_ALLOWED)
+    
+@api_view(['DELETE'])
+def delete_todo(request:Request,pk:int):
+    if request.method == 'DELETE':
+        instance = get_object_or_404(ToDo,pk=pk)
+        
+        print('adsfkj;lj;kjl;jl;j\n\n\n\n\n\n\n\n\\n\n\n\n\n\\n\n\n\n\\n\n\n')
+        instance.delete()
+        return Response("OK",HTTP_204_NO_CONTENT)
+
+    if request.method == "GET":
+        return Response(None,HTTP_405_METHOD_NOT_ALLOWED)
