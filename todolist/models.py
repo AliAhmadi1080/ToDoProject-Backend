0a263c5d610a8e484d17c6581deb99260d3df585
Ali <aliahmadi.xx90@gmail.comOmit>
2024-07-29 09:25:01 +0330

    I create nested srializers.

--- todolist/models.py
+++ todolist/models.py
@@ -28,17 +28,17 @@ class Status(models.Model):
 class ToDo(models.Model):
     title         = models.CharField('عنوان',max_length=63)
     subtitle     = models.CharField('زیر عنوان',max_length=63,null=True,blank=True)
-    user         = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name='کاربر',related_name='todos')
-    status       = models.ForeignKey("Status",on_delete=models.SET_DEFAULT,default='انجام نشده',verbose_name="وضعیت")
+    user         = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name='کاربر',related_name='user')
+    status       = models.ForeignKey("Status",on_delete=models.SET_DEFAULT,default=Status.objects.get(id=1),verbose_name="وضعیت",related_name='status')
     description  = models.TextField('توضیحات',null=True)
     created_at   = models.DateTimeField(auto_now_add=True,verbose_name='زمان شروع',editable=False)
-    tags         = models.ManyToManyField('Tag',verbose_name='تگ ها')
+    tags         = models.ManyToManyField('Tag',verbose_name='تگ ها',related_name='tags')
 
 
     # TODO:add subtasks to class
 
     def __str__(self) -> str:
-        return f"{self.name} -> {self.status}"
+        return f"{self.title} -> {self.status}"
 
     class Meta:
         verbose_name = 'وظیفه'
@@ -48,7 +48,7 @@ class ToDo(models.Model):
 
 class ToDoList(models.Model):
     name = models.CharField('نام',max_length=63)
-    todos = models.ManyToManyField(ToDo,verbose_name='وظیفه ها')
+    todos = models.ManyToManyField(ToDo,verbose_name='وظیفه ها',related_name='todos')
 
     def __str__(self) -> str:
         return self.name
