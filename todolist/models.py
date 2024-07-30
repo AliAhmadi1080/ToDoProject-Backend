aa865853a566cdec6dc1d4b7ffe1d2b1f6b3b6d6
Ali <aliahmadi.xx90@gmail.comOmit>
2024-07-30 09:41:01 +0330

    I add "user" field to ToDoList,Status,Tag models

--- todolist/models.py
+++ todolist/models.py
@@ -4,6 +4,7 @@ from datetime import date
 
 
 class Tag(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     name = models.CharField('نام تگ',max_length=63)
     created = models.DateField("زمان ایجاد شدن",auto_created=True,auto_now_add=True)
 
@@ -15,6 +16,7 @@ class Tag(models.Model):
         verbose_name_plural = 'تگ ها'    
 
 class Status(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     name = models.CharField(max_length=63,verbose_name="نام")
 
     def __str__(self) -> str:
@@ -28,17 +30,17 @@ class Status(models.Model):
 class ToDo(models.Model):
     title         = models.CharField('عنوان',max_length=63)
     subtitle     = models.CharField('زیر عنوان',max_length=63,null=True,blank=True)
-    user         = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name='کاربر',related_name='todos')
-    status       = models.ForeignKey("Status",on_delete=models.SET_DEFAULT,default='انجام نشده',verbose_name="وضعیت")
+    user         = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name='کاربر',related_name='user')
+    status       = models.ForeignKey("Status",on_delete=models.SET_DEFAULT,default=Status.objects.get(id=1),verbose_name="وضعیت",related_name='status')
     description  = models.TextField('توضیحات',null=True)
     created_at   = models.DateTimeField(auto_now_add=True,verbose_name='زمان شروع',editable=False)
-    tags         = models.ManyToManyField('Tag',verbose_name='تگ ها')
+    tags         = models.ManyToManyField('Tag',verbose_name='تگ ها',related_name='tags')
 
 
     # TODO:add subtasks to class
 
     def __str__(self) -> str:
-        return f"{self.name} -> {self.status}"
+        return f"{self.title} -> {self.status}"
 
     class Meta:
         verbose_name = 'وظیفه'
@@ -47,8 +49,9 @@ class ToDo(models.Model):
 
 
 class ToDoList(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     name = models.CharField('نام',max_length=63)
-    todos = models.ManyToManyField(ToDo,verbose_name='وظیفه ها')
+    todos = models.ManyToManyField(ToDo,verbose_name='وظیفه ها',related_name='todos')
 
     def __str__(self) -> str:
         return self.name
