bd5afb4a5dc96228b9c0f62311b83247cafdb6e6
Ali <aliahmadi.xx90@gmail.comOmit>
2024-07-30 15:44:30 +0330

    I setup JWT authentication.

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -125,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Rest Framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
--- ToDo/urls.py
+++ ToDo/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-
+    path('api-auth/', include('account.urls')),
     path('todo/', include('todolist.urls')),
 ]
