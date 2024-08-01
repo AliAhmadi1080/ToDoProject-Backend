ffc458f6baa179e0b9631ab11bc6c21668702820
Ali <aliahmadi.xx90@gmail.comOmit>
2024-08-01 10:35:43 +0330

    I create a serializer for "CustomeUser" model.

--- todolist/serializers.py
+++ todolist/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework.serializers import ModelSerializer
+from account.models import CustomeUser
 from .models import ToDo
-class ToDoSirializer(ModelSerializer):
+
+
+class ToDoSerializer(ModelSerializer):
     
     class Meta:
         model = ToDo
         fields = '__all__'
+
+class CustomeUserSerializer(ModelSerializer):
+
+    class Meta:
+        model = CustomeUser
+        fields = ('id','username','email')
 
 
 
