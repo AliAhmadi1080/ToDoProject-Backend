6ed832712f667967c8cc91b7ff7a1f9e9e5565ed
Ali <aliahmadi.xx90@gmail.comOmit>
2024-08-01 10:22:38 +0330

    I set "authentication_classes" and "permission_classes" for "ToDoGenericsDetail" view.

--- todolist/views.py
+++ todolist/views.py
@@ -6,46 +6,10 @@ from rest_framework import mixins, generics
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.status import *
-
+from rest_framework.permissions import IsAuthenticated 
+from rest_framework_simplejwt.authentication import JWTAuthentication
 User = get_user_model()
-# class ToDoList(APIView):
-#     def get(self,request:Request):
-#         todos = ToDo.objects.all()
-        
-#         serialized_todos = ToDoSirializer(todos,many=True)
-#         todos_data = serialized_todos.data
-#         return Response(todos_data,HTTP_200_OK)
-    
-#     def post(self,request:Request):
-#         instance = ToDoSirializer(data=request.data,many=False)
-#         if instance.is_valid():
-#             instance.save()
-#             return Response(instance.data,HTTP_201_CREATED)
-#         return Response(None,HTTP_400_BAD_REQUEST)
 
-        
-# class ToDoDetail(APIView):
-#     def get(self,request:Request,pk:int):
-#         todos = get_object_or_404(ToDo,pk=pk)
-    
-#         serialized_todos = ToDoSirializer(todos)
-#         todos_data = serialized_todos.data
-#         return Response(todos_data,HTTP_200_OK)
-
-#     def put(self,request:Request,pk:int):
-#         instance = get_object_or_404(ToDo,pk=pk)
-#         instance = ToDoSirializer(instance,data=request.data,many=False)
-#         if instance.is_valid():
-#             instance.save()
-#             return Response(instance.data,HTTP_201_CREATED)
-#         return Response(None,HTTP_400_BAD_REQUEST)
-
-#     def delete(self,request:Request,pk:int):
-
-#         instance = get_object_or_404(ToDo,pk=pk)
-#         instance.delete()
-#         return Response("OK",HTTP_204_NO_CONTENT)
-    
 class ToDoList(APIView):
     def get(self,request:Request):
         all_todos = ToDo.objects.all()
@@ -61,8 +25,9 @@ class ToDoList(APIView):
         return Response(None,HTTP_400_BAD_REQUEST)
 
     
-class ToDoGenericsDetail(generics.RetrieveUpdateDestroyAPIView):
-
+class ToDoGenericsDetail(generics.DestroyAPIView,generics.UpdateAPIView):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
     queryset = ToDo.objects.all()
     serializer_class = ToDoSirializer
 
