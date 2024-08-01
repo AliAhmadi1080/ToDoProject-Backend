d86fc1f1f06bd57cdd9649202a9ec1f7bb7c1ec2
Ali <aliahmadi.xx90@gmail.comOmit>
2024-08-01 11:16:09 +0330

    I change USERNAME_FIELD='username' to USERNAME_FIELD='email'

--- account/models.py
+++ account/models.py
@@ -4,3 +4,6 @@ from django.utils.translation import gettext_lazy as _
 
 class CustomeUser(AbstractUser):
     email = models.EmailField('ایمیل', blank=False,null=False,unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
