1f91cbb493e0eb66f2223477f76d43c236906fea
Ali <aliahmadi.xx90@gmail.comOmit>
2024-08-03 22:12:10 +0330

    I create create API for ToDo and ToDoList

--- todolist/urls.py
+++ todolist/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.ToDoList.as_view()),
-    path('usertodo',views.UserToDo.as_view()),
+    path('',views.ToDoAppend.as_view()),
+    path('usertodo',views.UserToDoList.as_view()),
+    path('createtodolist',views.ToDoListCreate.as_view()),
     path('<int:pk>/',views.ToDoGenericsDetail.as_view()),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
-from .serializers import ToDoSerializer, CustomeUserSerializer
-from .models import ToDo
+from .serializers import ToDoSerializer,ToDoListSerializer
+from .models import ToDo, ToDoList
 from django.contrib.auth import get_user_model
 from rest_framework import generics
 from rest_framework.response import Response
@@ -11,43 +11,35 @@ from rest_framework_simplejwt.authentication import\
                                      JWTAuthentication
 User = get_user_model()
 
-class ToDoList(APIView):
-    def get(self,request:Request):
-        all_todos = ToDo.objects.all()
-        serialized_data = ToDoSerializer(all_todos,many=True)
-        return Response(serialized_data.data,HTTP_200_OK)
-    
+class ToDoCreate(APIView):
     def post(self,request:Request):
-        instance = ToDoSerializer(data=request.data,many=False)
+        instance = ToDoSerializer(data=request.data)
         if instance.is_valid():
             instance.save()
             return Response(instance.data,HTTP_201_CREATED)
-            
-        return Response(None,HTTP_400_BAD_REQUEST)
+        return Response(None,HTTP_406_NOT_ACCEPTABLE)
 
-    
+class ToDoListCreate(APIView):
+    def post(self,request:Request):
+        instance = ToDoListSerializer(data=request.data)
+        if instance.is_valid():
+            instance.save()
+            return Response(instance.data, HTTP_201_CREATED)
+        return Response(None, HTTP_400_BAD_REQUEST)
+
 class ToDoGenericsDetail(generics.DestroyAPIView,generics.UpdateAPIView):
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = ToDo.objects.all()
     serializer_class = ToDoSerializer
 
-class UserToDo(APIView):
+class UserToDoList(APIView):
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
     
     def get(self,request:Request):
-        try:
-            this_user = request.user
-            these_todos = ToDo.objects.filter(user=this_user)
-            serialized_data = ToDoSerializer(these_todos,many=True)
-            user = CustomeUserSerializer(this_user)
-        except:
-            return Response({},HTTP_400_BAD_REQUEST)
-        return Response({'todo':serialized_data.data,'user':user.data},HTTP_200_OK)
-    
-
-    
-    
-
+        this_user = request.user
+        these_todos = ToDoList.objects.filter(user=this_user)
+        serialized_data = ToDoListSerializer(these_todos,many=True)
+        return Response(serialized_data.data,HTTP_200_OK)
     
