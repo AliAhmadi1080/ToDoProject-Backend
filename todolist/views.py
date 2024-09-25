170ac02379c43cbf0cea210a24f84060861e4ea0
Ali <aliahmadi.xx90@gmail.comOmit>
2024-09-25 17:08:23 +0330

    I fix a bug from ToDoGenericsDetail class and update method

--- todolist/views.py
+++ todolist/views.py
@@ -1,36 +1,38 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
-from .serializers import ToDoSerializer,ToDoListSerializer,\
-                            TagSerializer, StatusSerializer
+from .serializers import ToDoSerializer, ToDoListSerializer, \
+    TagSerializer, StatusSerializer
 from .models import ToDo, ToDoList, Tag, Status
 from django.contrib.auth import get_user_model
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.status import *
-from rest_framework.permissions import IsAuthenticated 
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import\
-                                     JWTAuthentication
+    JWTAuthentication
 User = get_user_model()
 
+
 class ToDoCreate(APIView):
-    def post(self,request:Request):
+    def post(self, request: Request):
         request.data['user'] = request.user.id
-        
+
         instance = ToDoSerializer(data=request.data)
-        
+
         if instance.is_valid():
             instance.save()
-            return Response(instance.data,HTTP_201_CREATED)
-        return Response(None,HTTP_406_NOT_ACCEPTABLE)
+            return Response(instance.data, HTTP_201_CREATED)
+        return Response(None, HTTP_406_NOT_ACCEPTABLE)
+
 
 class ToDoListAPI(APIView):
-    def get(self,request:Request):
+    def get(self, request: Request):
         these_todos = ToDoList.objects.all()
         instance = ToDoListSerializer(these_todos, many=True)
         return Response(instance.data, HTTP_200_OK)
-    
-    def post(self,request:Request):
+
+    def post(self, request: Request):
         request.data['user'] = request.user.id
         instance = ToDoListSerializer(data=request.data)
         if instance.is_valid():
@@ -38,84 +40,88 @@ class ToDoListAPI(APIView):
             return Response(instance.data, HTTP_201_CREATED)
         return Response(None, HTTP_400_BAD_REQUEST)
 
+
 class ToDoGenericsDetail(generics.RetrieveUpdateDestroyAPIView):
-    
+
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = ToDo.objects.all()
     serializer_class = ToDoSerializer
 
-    def update(self, request:Request, *args, **kwargs):
+    def update(self, request: Request, *args, **kwargs):
         try:
             request.data._mutable = True
         except:
             pass
         request.data['user'] = request.user.id
-        todo_object:ToDo = self.get_object()
-        request.data['todo_list'] = todo_object.todo_list.id
-        instance = ToDoSerializer(instance=todo_object, data=request.data)
-        
-        if instance.is_valid():
-            instance.save()
-            instance:ToDo = instance.instance
+        todo_object: ToDo = self.get_object()
+        serializers_instance = ToDoSerializer(
+            instance=todo_object, data=request.data)
+
+        if serializers_instance.is_valid():
+            serializers_instance.save()
+            instance: ToDo = serializers_instance.instance
             instance.tags.clear()
             for i in request.data.pop('tags'):
                 tag = Tag.objects.get(id=i)
                 instance.tags.add(tag)
             instance.save()
-            return Response('good',HTTP_201_CREATED)
+            return Response(serializers_instance.data, HTTP_201_CREATED)
         return super().update(request, *args, **kwargs)
+
 
 class UserToDoList(APIView):
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
-    
-    def get(self,request:Request):
+
+    def get(self, request: Request):
         these_todos = ToDoList.objects.filter(user=request.user)
-        serialized_data = ToDoListSerializer(these_todos,many=True)
-        return Response(serialized_data.data,HTTP_200_OK)
-    
+        serialized_data = ToDoListSerializer(these_todos, many=True)
+        return Response(serialized_data.data, HTTP_200_OK)
+
+
 class TagToDo(APIView):
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
-    
-    def get(self,request:Request,slug):
+
+    def get(self, request: Request, slug):
         tag = get_object_or_404(Tag, slug=slug)
-        these_todos = ToDo.objects.filter(user=request.user,tags=tag)
-        serialized_data = ToDoSerializer(these_todos,many=True)
-        return Response(serialized_data.data,HTTP_200_OK)
-    
+        these_todos = ToDo.objects.filter(user=request.user, tags=tag)
+        serialized_data = ToDoSerializer(these_todos, many=True)
+        return Response(serialized_data.data, HTTP_200_OK)
+
+
 class UserTag(APIView):
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
 
-    def get(self, request:Request):
+    def get(self, request: Request):
         tags = Tag.objects.filter(user=request.user)
         serialized_data = TagSerializer(tags, many=True)
-        return Response(serialized_data.data,HTTP_200_OK)
-    
-    def post(self, request:Request):
+        return Response(serialized_data.data, HTTP_200_OK)
+
+    def post(self, request: Request):
         request.data['user'] = request.user.id
         instance = TagSerializer(data=request.data)
         if instance.is_valid():
             instance.save()
-            return Response(instance.data,HTTP_201_CREATED)
-        return Response(None,HTTP_406_NOT_ACCEPTABLE)
+            return Response(instance.data, HTTP_201_CREATED)
+        return Response(None, HTTP_406_NOT_ACCEPTABLE)
+
 
 class UserStatus(APIView):
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
 
-    def get(self, request:Request):
+    def get(self, request: Request):
         tags = Status.objects.filter(user=request.user)
         serialized_data = StatusSerializer(tags, many=True)
-        return Response(serialized_data.data,HTTP_200_OK)
-    
-    def post(self, request:Request):
+        return Response(serialized_data.data, HTTP_200_OK)
+
+    def post(self, request: Request):
         request.data['user'] = request.user.id
         instance = StatusSerializer(data=request.data)
         if instance.is_valid():
             instance.save()
-            return Response(instance.data,HTTP_201_CREATED)
-        return Response(None,HTTP_406_NOT_ACCEPTABLE)
-    
+            return Response(instance.data, HTTP_201_CREATED)
+        return Response(None, HTTP_406_NOT_ACCEPTABLE)
